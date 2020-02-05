184b2e7b9ba8376d0e1205d89c3067899ac8de5b
monicaoyugi <monicaoyugi@gmail.com>
2020-02-05 12:18:10 +0300

    import security key

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
