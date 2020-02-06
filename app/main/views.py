d46a6ad251f73169f4bcaaa742466e133111c199
monicaoyugi <monicaoyugi@gmail.com>
2020-02-06 11:29:26 +0300

    rendered articles template to main route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from . import main
-from app.request import get_sources
+from app.request import get_sources, get_articles
 
 
 @main.route('/')
@@ -10,20 +10,17 @@ def index():
     return render_template('index.html', sources=sources)
 
 
-
-
 # @main.route('/articles', methods=["POST", "GET"])
 # def articles_page():
 #     if request.method == 'POST':
 #         search = request.form.get("search")
 #         articles = NewsRequest.get_articles(search)
 #     else:
-#         articles = NewsRequest.get_articles("tech")
+#         articles = New  sRequest.get_articles("tech")
 #     return render_template('articles.html', articles=articles)
 #
 #
-# @main.route('/article/<id>')
-# def source_article(id):
-#     source_articles = NewsRequest.get_article_by_source(id)
-#     source = id
-#     return render_template('articles_display.html', source_articles=source_articles, )
+@main.route('/article/<id>')
+def source_article(id):
+    source_articles = get_articles(id)
+    return render_template('articles_display.html', source_articles=source_articles)
